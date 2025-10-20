6955e222a1c1385e2e6f510c408c154e7dc31a0e
Leewonno <dldnjssh123@naver.com>
2025-10-20 09:06:06 +0900

    feat : read file 2

--- src/app/api/get/[id]/route.ts
+++ src/app/api/get/[id]/route.ts
@@ -1,22 +1,22 @@
-// import { NextResponse } from "next/server";
-// import fs from "fs";
-// import path from "path";
-// import matter from "gray-matter"; // frontmatter 파싱용
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter"; // frontmatter 파싱용
 
-// export async function GET(_: Request, { params }: { params: { id: string } }) {
-//   try {
-//     const filePath = path.join(process.cwd(), "posts", `${params.id}.md`);
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  try {
+    const filePath = path.join(process.cwd(), "posts", `${params.id}.md`);
 
-//     if (!fs.existsSync(filePath)) {
-//       return NextResponse.json({ message: "파일을 찾을 수 없습니다." }, { status: 404 });
-//     }
+    if (!fs.existsSync(filePath)) {
+      return NextResponse.json({ message: "파일을 찾을 수 없습니다." }, { status: 404 });
+    }
 
-//     const fileContent = fs.readFileSync(filePath, "utf-8");
-//     const { data: meta, content } = matter(fileContent);
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    const { data: meta, content } = matter(fileContent);
 
-//     return NextResponse.json({ meta, content });
-//   } catch (error: any) {
-//     console.error("파일 읽기 오류:", error);
-//     return NextResponse.json({ error: error.message }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ meta, content });
+  } catch (error: any) {
+    console.error("파일 읽기 오류:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
