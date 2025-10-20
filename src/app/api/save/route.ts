7c82f97a3d4e450c3f0c20c0943ccd2ad86d4821
Leewonno <dldnjssh123@naver.com>
2025-10-20 23:54:34 +0900

    feat : create

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -1,13 +1,26 @@
 import { NextResponse } from "next/server";
 import { writeFile, mkdir } from "fs/promises";
 import path from "path";
+import { readdirSync } from "fs";
 
 export async function POST(request: Request) {
   const { filename, content, meta } = await request.json();
 
   if (!filename || !content) {
-    return NextResponse.json({ message: "Filename and content required" }, { status: 400 });
+    return NextResponse.json({ message: "제목과 내용을 입력해주세요." }, { status: 400 });
   }
+
+  const dir = path.join(process.cwd(), "posts");
+  await mkdir(dir, { recursive: true });
+
+  // posts 폴더 안 파일 목록 확인
+  const files = readdirSync(dir);
+
+  // 숫자 파일명 추출 (["1.md", "2.md"] → [1, 2])
+  const ids = files.map((file) => parseInt(path.parse(file).name)).filter((num) => !isNaN(num));
+
+  // 새 ID
+  const newId = ids.length > 0 ? Math.max(...ids) + 1 : 1
 
   // frontmatter 생성
   let frontmatter = "---\n";
@@ -19,11 +32,8 @@ export async function POST(request: Request) {
   frontmatter += "---\n\n";
 
   const fileContent = frontmatter + content;
-
-  const postsDir = path.join(process.cwd(), "posts");
-  await mkdir(postsDir, { recursive: true });
-  const filePath = path.join(postsDir, `${filename}.md`);
+  const filePath = path.join(dir, `${newId}.md`);
   await writeFile(filePath, fileContent, "utf-8");
 
-  return NextResponse.json({ message: "File saved successfully", path: filePath });
+  return NextResponse.json({ message: "저장되었습니다.", path: filePath });
 }
--- src/widgets/write/components/WriteEditor.tsx
+++ src/widgets/write/components/WriteEditor.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import styled from 'styled-components';
 
@@ -8,11 +9,19 @@ import styled from 'styled-components';
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
 const Box = styled.div`
-  width: 1000px;
+  width: 100%;
 `
 
 const TitleInput = styled.input`
   width: 100%;
+  border: none;
+  margin-bottom: 20px;
+  outline: none;
+  font-size: 18px;
+  color: var(--black);
+  background-color: var(--white);
+  padding-bottom: 5px;
+  border-bottom: 1px solid var(--gray);
 `
 
 const SaveButton = styled.button`
@@ -20,6 +29,8 @@ const SaveButton = styled.button`
 `
 
 export default function WriteEditor() {
+  const router = useRouter()
+
   const [title, setTitle] = useState<string | undefined>("");
   const [content, setContent] = useState<string | undefined>("");
 
@@ -32,20 +43,19 @@ export default function WriteEditor() {
     });
     const data = await res.json();
     alert(data.message);
+    router.push('/')
   };
 
   return (
     <Box>
       <TitleInput
+        placeholder='제목'
         type="text"
         onChange={(e) => setTitle(e.target.value)}
       />
       <MDEditor
         value={content}
         onChange={(e) => setContent(e)}
-        style={{
-          // borderRadius: "10px",
-        }}
       />
       <SaveButton onClick={() => savePost()}>저장</SaveButton>
     </Box>
